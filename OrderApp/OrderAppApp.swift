83406535b8b69590858da2c98acb22f43c3140a2
Эдуард Птушко <ptushko@gmail.com>
2023-12-24 17:45:10 +0300

    Initial Commit

---
+++ OrderApp/OrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppApp.swift
+//  OrderApp
+//
+//  Created by Eduard Ptushko on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
